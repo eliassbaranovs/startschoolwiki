d86a14f6b0f2c8eaa3ace7dfe87e201212230a05
eliassbaranovs <82933682+eliassbaranovs@users.noreply.github.com>
2024-10-04 20:16:33 +0300

    Deploy website - based on 305f56af37d8b8045001edc59fa04903358b5c02

---
+++ assets/js/acecf23e.d4dac8dd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/startschoolwiki/blog","blogTitle":"Blog","authorsListPath":"/startschoolwiki/blog/authors"}')}}]);
